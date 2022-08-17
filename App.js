433fd5e04ff5232d1a10f1064e960d8038babaf9
Varsha Bhagat <varshabhagat@Varshas-MacBook-Air.local>
2022-08-17 12:01:09 +0530

    inital page

--- App.js
+++ App.js
@@ -1,114 +1,122 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
-import type {Node} from 'react';
 import {
   SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
+  Button,
   Text,
-  useColorScheme,
   View,
+  StyleSheet,
+  Pressable,
+  Image
 } from 'react-native';
+import { useRecoilState, useResetRecoilState } from "recoil";
+import { playerOrder } from "./config/constants";
+import * as states from "./recoil/atoms";
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+function PlayerButton({ count }) {
+  const setCurrentPlayer = useRecoilState(states.currentPlayerState)[1];
+  const setCurrentPlayersList = useRecoilState(
+    states.currentPlayersListState
+  )[1];
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+  const resetBlockState = useResetRecoilState(states.allBlockState);
+  const resetCoinState = useResetRecoilState(states.allCoinState);
+  const resetDiceState = useResetRecoilState(states.currentDiceState);
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+  // Event handler for button onClick.
+  const handleClick = () => {
+    console.log("inside", count)
+    const randomNum = Math.floor(Math.random() * 2);
+    const playersList =
+      count === 2
+        ? [playerOrder[randomNum], playerOrder[randomNum + 2]]
+        : count === 3
+        ? playerOrder.slice(randomNum, randomNum + 3)
+        : [...playerOrder];
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+    // reset previous states
+    resetBlockState();
+    resetCoinState();
+    resetDiceState();
+
+    setCurrentPlayersList([...playersList]);
+    setCurrentPlayer(playersList[0]);
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          {/* <Section title="Step One">
-            heloo
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-            hellor
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section> */}
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+    <Pressable style={styles.roundButton} onPress={handleClick}>
+      <Text style={styles.text}>{count}</Text>
+    </Pressable>
+  );
+}
+
+const App = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>Welcome to Ludo</Text>
+      <Image
+            style ={styles.img}
+            source={require('./img/ludo.gif')}
+          />
+      <Text style={styles.subtitle}>No of Players</Text>
+      <View className={styles.gameSetup}>
+        {[2, 3, 4].map((i) => (
+          <PlayerButton
+            key={i}
+            count={i}
+          />
+        ))}
+      </View>
     </SafeAreaView>
   );
 };
 
+export default App;
+
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    alignItems: 'center',
+    backgroundColor: 'white',
+    flex: 1
   },
-  sectionTitle: {
-    fontSize: 24,
+  gameSetup: {
+    flex: 1,
+    flexDirection: 'row'
+  },
+  img: {
+    width: 250,
+    height: 250,
+    marginBottom: 40,
+  },
+  title: {
+    color: 'black',
+    fontWeight: 'bold',
+    fontSize: 30,
+    margin: 60,
+    textAlign: 'center'
+  },
+  subtitle: {
+    color: 'black',
     fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
     fontSize: 18,
-    fontWeight: '400',
+    margin: 20,
+    textAlign: 'center'
   },
-  highlight: {
-    fontWeight: '700',
+  roundButton: {
+    width: 50,
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 10,
+    borderRadius: 100,
+    backgroundColor: 'black',
+    margin: 10,
+  },
+  text: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: 'white',
+    textAlign: 'center'
   },
 });
-
-export default App;
--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 /**
  * @format
  */
-
+import React from 'react';
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import { RecoilRoot } from "recoil";
 
-AppRegistry.registerComponent(appName, () => App);
+const Ludo = () =>(
+    <RecoilRoot>
+      <App />
+    </RecoilRoot>)
+
+
+
+AppRegistry.registerComponent(appName, () => Ludo);
